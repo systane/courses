d419e00a5998aa44aeee83703d2538dde2cab8de
Luis Fernando <lf.silva@outlook.com>
2018-12-15 11:02:05 -0200

    Added logic to update the store information

--- nodejs/controllers/storeController.js
+++ nodejs/controllers/storeController.js
@@ -21,3 +21,22 @@ exports.getStores = async(req, res) => {
     const stores = await Store.find();
     res.render('stores', {title: 'Stores', stores});
 }
+
+exports.editStore = async(req, res) => {
+    const store = await(Store.findOne({_id: req.params.id}));
+    //TODO Confirm they are the owner of the store
+
+    res.render('editStore', {title: `Edit ${store.name}`, store});
+}
+
+exports.updateStore = async(req, res) => {
+    const store = await Store.findOneAndUpdate({_id: req.params.id}, req.body, {
+        new: true, //return the new store instead of the old one
+        runValidators: true //force mongobd Validate the fields
+    }).exec();
+
+    req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store -></a>`);
+
+    res.redirect(`/stores/${store._id}/edit`);
+    
+}
