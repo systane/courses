3932677576ff97c61634a360668bd4d6c37c08a5
Luis Fernando <lf.silva@outlook.com>
2018-12-16 16:16:11 -0200

    Added form to register a new user and log in

---
+++ nodejs/controllers/UserController.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+// const Store = mongoose.model('Store');
+
+exports.loginForm = (req, res) => {
+    res. render('login', {title: 'Login'});
+}
+
+exports.registerForm = (req, res) => {
+    res. render('register', {title: 'Register'});
+}
+
+exports.validateRegister = (req, res, next) => {
+    req.sanitizeBody('name');
+    req.checkBody('name', 'You must supply a name!').notEmpty();
+    
+    req.sanitizeBody('email').normalizeEmail({
+        remove_dots: false,
+        remove_extension: false,
+        gmail_remove_subaddress: false,
+    });
+    req.checkBody('email', 'That Email is not valida!').notEmpty().isEmail();
+
+    req.checkBody('password', 'Password cannot be blank!').notEmpty();
+    req.checkBody('password-confirm', 'Confirmed Password cannot be blank!').notEmpty();
+    req.checkBody('password-confirm', 'Oops! Your passwords do not match').equals(req.body.password);
+
+    const errors = req.validationErrors();
+
+    if(errors){
+        req.flash('error', errors.map(err => err.msg));
+        res.render('register', {title: 'Register', body: req.body, flashes: req.flash()});
+        return; //stop the function from running
+    }
+    next();//there were no errors - continue with the process of save the register
+
+}
---
+++ nodejs/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const md5 = require('md5');
+const validator = require('validator');
+const mongodbErrorHandler = require('mongoose-mongodb-errors');
+const passportLocalMongoose = require('passport-local-mongoose');
+mongoose.Promise = global.Promise;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        validate: [validator.isEmail, 'Invalid Email Address'],
+        require: 'Please supply an email address'
+    },
+    name: {
+        type: String,
+        required: 'Please supply a name',
+        trim: true
+    }
+});
+
+
+userSchema.plugin(passportLocalMongoose, {usernameField: 'email'}); //plugin to simplify the building username and password login with Passport (authentication middleware for Node.js.)
+userSchema.plugin(mongodbErrorHandler); //transform mongodb unfriendly errors to friendly erros in order to show to the user
+
+module.exports = mongoose.model('User', userSchema);
