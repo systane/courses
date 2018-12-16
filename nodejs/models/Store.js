04e6235736848f03a41d7ca8e8282d1d9df1e0ef
Luis Fernando <lf.silva@outlook.com>
2018-12-15 22:03:20 -0200

    added unique slug to every new Store saved

--- nodejs/models/Store.js
+++ nodejs/models/Store.js
@@ -36,12 +36,20 @@ const storeSchema = new mongoose.Schema({
 
 });
 
-storeSchema.pre('save', function (next) {
+storeSchema.pre('save', async function (next) {
     if(!this.isModified('name')){
         next(); //skip it
         return; //stop this function from running
     }
     this.slug = slug(this.name);
+
+    const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)`, 'i');
+    const storesWithSlug = await this.constructor.find({slug: slugRegEx});
+
+    if(storesWithSlug.length){
+        this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+    }
+
     next();
     //TODO make more resiliant so slugs are unique
 });
