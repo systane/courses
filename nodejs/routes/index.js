2fb73228c15f1c37ef0d96734d33e64f2e37c4e4
Luis Fernando <lf.silva@outlook.com>
2018-12-29 18:04:19 -0200

    added logic to make review off stories

--- nodejs/routes/index.js
+++ nodejs/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
+const reviewController = require('../controllers/reviewController');
 const {catchErrors} = require('../handlers/errorHandlers');
 
 router.get('/', catchErrors(storeController.getStores));
@@ -71,7 +72,7 @@ router.get('/reverse/:name', (req, res) => {
 
 router.get('/map', storeController.mapPage);
 router.get('/hearts', catchErrors(storeController.getHearts));
-
+router.post('/reviews/:id', authController.isLoggedIn, catchErrors(reviewController.addReview));
 
 
 /*
