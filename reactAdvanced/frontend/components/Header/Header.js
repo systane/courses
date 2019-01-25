2daa4c99d8ad253e856b0eaaa09c83a493a18dbd
Luis Fernando <lf.silva@outlook.com>
2019-01-25 14:58:23 -0200

    adicionado o prisma do projeto para auxiliar nas consultas em graphql :)

--- reactAdvanced/frontend/components/Header/Header.js
+++ reactAdvanced/frontend/components/Header/Header.js
@@ -1,6 +1,25 @@
-import Nav from '../Nav/Nav';
-import Link from 'next/link';
 import { Logo, StyledHeader } from './HeaderStyle';
+import Link from 'next/link';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+
+
+import Nav from '../Nav/Nav';
+
+
+//Configuring the loading bar
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+};
+
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+};
+
+Router.onRouteChangeError = () => {
+  NProgress.done();
+};
+
 
 const Header = () => (
   <StyledHeader>
