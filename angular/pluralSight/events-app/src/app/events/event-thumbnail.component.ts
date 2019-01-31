5e6941da728b8101a9fae3430c2eb23dfe5b1f42
Luis Fernando <lf.silva@outlook.com>
2019-01-31 17:00:53 -0200

    avanços no curso de angular

--- angular/pluralSight/events-app/src/app/events/event-thumbnail.component.ts
+++ angular/pluralSight/events-app/src/app/events/event-thumbnail.component.ts
@@ -4,14 +4,17 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   selector: 'event-thumbnail',
   template: `
     <div class="well hoverwell thumbnail">
-      <h2>{{event.name}}</h2>
+      <h2>{{event?.name}}</h2>
       <div>Date: {{event.date}}</div>
       <div>Time: {{event.time}}</div>
       <div>Price: \${{ event.price }}</div>
-      <div>
+      <div *ngIf="event?.location">
         <span>Location: {{event.location.address}}</span>
         <span>&nbsp;</span>
         <span>{{event.location.city}}, {{event.location.country}}</span>
+      </div>
+      <div *ngIf="event?.onlineUrl">
+        Online URL: {{event.onlineUrl}}
       </div>
       <button class="btn btn-primary" (click)="handleClickMe()">Click me!</button>
     </div>
