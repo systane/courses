c2c17f7bb9b9fdb4b3f776b573d03ec4c42e1ec6
Luis Fernando <lf.silva@outlook.com>
2019-04-15 13:42:09 -0300

    Added service to test the zuul

---
+++ springCloud/pluralSightEssencials/goodbye-service/src/main/java/com/example/goodbyeservice/GoodbyeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.goodbyeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodbyeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodbyeServiceApplication.class, args);
+	}
+
+}
