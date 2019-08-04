de57f68d3204458a2bc073856a2ac6e441a27ca5
Nando <lf.silva@outlook.com>
2019-08-04 10:44:30 -0300

    Add project to test kubernetes

---
+++ kubernetes/project/src/main/java/com/example/kubernetes/KubernetesEndpoint.java
@@ -0,0 +1,19 @@
+package com.example.kubernetes;
+
+import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class KubernetesEndpoint {
+
+    private final Environment environment;
+
+    @GetMapping(value = "/helloKubernetes")
+    public String helloKubernetes(){
+        String port = environment.getProperty("local.server.port");
+        return "Hello Kubernetes from port " + port + " :)";
+    }
+}
