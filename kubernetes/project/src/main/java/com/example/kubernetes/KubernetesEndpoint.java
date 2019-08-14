30f2ac3edd2641663155844dce612961f904864e
Luis Fernando <luis.silva@accurate.com.br>
2019-08-14 11:42:33 -0300

    Hands on: project

--- kubernetes/project/src/main/java/com/example/kubernetes/KubernetesEndpoint.java
+++ kubernetes/project/src/main/java/com/example/kubernetes/KubernetesEndpoint.java
@@ -5,6 +5,9 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 @AllArgsConstructor
 public class KubernetesEndpoint {
@@ -13,7 +16,14 @@ public class KubernetesEndpoint {
 
     @GetMapping(value = "/helloKubernetes")
     public String helloKubernetes() {
-        String port = environment.getProperty("local.server.port");
-        return "Hello Kubernetes from port " + port + " :)";
+        try {
+            InetAddress inetAddress = InetAddress.getLocalHost();
+            String port = environment.getProperty("local.server.port");
+
+            return "Hello Kubernetes from address:" + inetAddress.getHostAddress() + ":"+ port +  " hostname: " + inetAddress.getHostName() +" :)";
+        } catch (UnknownHostException e) {
+            return "Cannot get HostAddress :(";
+        }
+
     }
 }
