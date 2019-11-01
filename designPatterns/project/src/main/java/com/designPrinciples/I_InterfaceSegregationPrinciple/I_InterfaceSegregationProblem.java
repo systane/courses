b0c33e55dbb9d1051c642f2f1881ce03912b332a
Nando <lf.silva@outlook.com>
2019-11-01 16:25:45 -0300

    ISP

--- designPatterns/project/src/main/java/com/designPrinciples/I_InterfaceSegregationPrinciple/I_InterfaceSegregationProblem.java
+++ designPatterns/project/src/main/java/com/designPrinciples/I_InterfaceSegregationPrinciple/I_InterfaceSegregationProblem.java
@@ -1,4 +1,44 @@
 package com.designPrinciples.I_InterfaceSegregationPrinciple;
 
 public class I_InterfaceSegregationProblem {
+
+    private interface CellPhone {
+        void ring();
+        void call();
+        void takePicture();
+    }
+
+    private static class OldNokiaTijolao implements CellPhone {
+        @Override
+        public void ring() {
+            //Ring
+        }
+
+        @Override
+        public void call() {
+            //Call
+        }
+
+        @Override
+        public void takePicture() {
+            throw new UnsupportedOperationException("Operation not supported :(");
+        }
+    }
+
+    private static class Xiaomi implements CellPhone {
+        @Override
+        public void ring() {
+
+        }
+
+        @Override
+        public void call() {
+
+        }
+
+        @Override
+        public void takePicture() {
+
+        }
+    }
 }
--- designPatterns/project/src/main/java/com/designPrinciples/I_InterfaceSegregationPrinciple/I_InterfaceSegregationSolution.java
+++ designPatterns/project/src/main/java/com/designPrinciples/I_InterfaceSegregationPrinciple/I_InterfaceSegregationSolution.java
@@ -1,4 +1,42 @@
 package com.designPrinciples.I_InterfaceSegregationPrinciple;
 
 public class I_InterfaceSegregationSolution {
+
+    private interface NormalCellPhone {
+        void ring();
+        void call();
+    }
+
+    private interface AdvancedCellPhone {
+        void takePicture();
+    }
+
+    private static class OldNokiaTijolao implements NormalCellPhone {
+        @Override
+        public void ring() {
+            //Ring
+        }
+
+        @Override
+        public void call() {
+            //Call
+        }
+    }
+
+    private static class Xiaomi implements NormalCellPhone, AdvancedCellPhone {
+        @Override
+        public void ring() {
+
+        }
+
+        @Override
+        public void call() {
+
+        }
+
+        @Override
+        public void takePicture() {
+
+        }
+    }
 }
