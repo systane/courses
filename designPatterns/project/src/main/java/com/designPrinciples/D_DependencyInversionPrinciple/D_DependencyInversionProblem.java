9203717697100942aaa4601ab884a5e64e23a6c1
Nando <lf.silva@outlook.com>
2019-11-01 16:59:35 -0300

    DIP

--- designPatterns/project/src/main/java/com/designPrinciples/D_DependencyInversionPrinciple/D_DependencyInversionProblem.java
+++ designPatterns/project/src/main/java/com/designPrinciples/D_DependencyInversionPrinciple/D_DependencyInversionProblem.java
@@ -1,4 +1,14 @@
 package com.designPrinciples.D_DependencyInversionPrinciple;
 
 public class D_DependencyInversionProblem {
+
+    private static class Windows98Machine {
+        private final StandardKeyboard keyboard;
+
+        public Windows98Machine() {
+            this.keyboard = new StandardKeyboard();
+        }
+    }
+
+    private static class StandardKeyboard {}
 }
--- designPatterns/project/src/main/java/com/designPrinciples/D_DependencyInversionPrinciple/D_DependencyInversionSolution.java
+++ designPatterns/project/src/main/java/com/designPrinciples/D_DependencyInversionPrinciple/D_DependencyInversionSolution.java
@@ -1,4 +1,18 @@
 package com.designPrinciples.D_DependencyInversionPrinciple;
 
 public class D_DependencyInversionSolution {
+
+    private static class Windows98Machine {
+        private final Keyboard keyboard;
+
+        public Windows98Machine(Keyboard keyboard) {
+            this.keyboard = keyboard;
+        }
+    }
+
+    private static class StandardKeyboard implements Keyboard {}
+
+    private static class GamerKeyboard implements Keyboard {}
+
+    private interface Keyboard {}
 }
