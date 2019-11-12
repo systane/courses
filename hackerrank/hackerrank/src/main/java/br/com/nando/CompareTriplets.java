e295a2d475b2c947d2575de762ca5a84d5efdc59
Nando <lf.silva@outlook.com>
2019-11-12 08:35:59 -0300

    adjust formating

--- hackerrank/hackerrank/src/main/java/br/com/nando/CompareTriplets.java
+++ hackerrank/hackerrank/src/main/java/br/com/nando/CompareTriplets.java
@@ -1,65 +1,57 @@
 package br.com.nando;
 
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 public class CompareTriplets {
 
-        // Complete the compareTriplets function below.
-        static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
-            List<Integer> result = new ArrayList<>();
-            result.add(0, 0);
-            result.add(1 , 0);
+    // Complete the compareTriplets function below.
+    static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        List<Integer> result = new ArrayList<>();
+        result.add(0, 0);
+        result.add(1, 0);
 
-            Integer Apointuation = result.get(0);
-            Integer Bpointuation = result.get(1);
+        Integer Apointuation = result.get(0);
+        Integer Bpointuation = result.get(1);
 
-            for(int i = 0; i < a.size(); i++) {
+        for (int i = 0; i < a.size(); i++) {
 
 
-                if(a.get(i) > b.get(i)) {
-                    result.set(0, ++Apointuation);
-                }
-                else if (a.get(i) < b.get(i)) {
-                    result.set(1, ++Bpointuation);
-                }
-            }
-            return result;
+            if (a.get(i) > b.get(i))
+                result.set(0, ++Apointuation);
+            else if (a.get(i) < b.get(i))
+                result.set(1, ++Bpointuation);
         }
-
-        public static void main(String[] args) throws IOException {
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-            List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                    .map(Integer::parseInt)
-                    .collect(toList());
-
-            List<Integer> b = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                    .map(Integer::parseInt)
-                    .collect(toList());
-
-            List<Integer> result = compareTriplets(a, b);
-
-            bufferedWriter.write(
-                    result.stream()
-                            .map(Object::toString)
-                            .collect(joining(" "))
-                            + "\n"
-            );
-
-            bufferedReader.close();
-            bufferedWriter.close();
-        }
+        return result;
     }
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> b = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = compareTriplets(a, b);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
 }
