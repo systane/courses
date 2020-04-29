d22a1d49050ec877e8f4e65656db7263d1aa6523
Luis Fernando <luis.silva@zup.com.br>
2020-04-29 20:27:05 -0300

    vuejs

---
+++ vue/hello-world/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!--we can use the extended form: v-on:click="incrementYear" instead of @click=incrementYear -->
+    <button @click=incrementYear>
+      me clica
+    </button>
+    <HelloWorld :msg=msg :number=year />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data: () => ({
+    year: 2020,
+    msg: "Welcome to Your Vue.js App"
+  }),
+  methods: {
+    incrementYear() {
+      this.year++;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
