91f322a548910dc6405def0ed4264124345e586d
Luis Fernando <luis.silva@zup.com.br>
2020-07-13 23:04:28 -0300

    String template in Kotlin

--- kotlin/src/academy/learprogramming/helloworld/Equality.kt
+++ kotlin/src/academy/learprogramming/helloworld/Equality.kt
@@ -24,16 +24,39 @@ fun main(args: Array<String>) {
     println(student2 != student3)
     println(student2 !== student3)
 
+    println("operator for instance of")
+    val something: Any = student4 //creates a something val with the type Any and initialize it as a student4 instance
+    if(something is Student){ // !is is the negative operator for instance of
+        val newStudent = something as Student
+        println(newStudent.toString())
+    }
+
+    val change = 4.22
+    //how print $change if we declared a variable called change? Simple, just use \$change to escape the $ sign
+    println("To show the value of change, we use \$change")
+    println("Your change is $$change")
+    println("Your change is $change")
+
+    val numerator = 20
+    val denominator = 10
+    println("the value of $numerator divided by $denominator is ${numerator/denominator}")
+    println("the name of the student4 is ${student4.name}")
+
 }
 
 class Student(var name: String, val id: Int){
 
     override fun equals(other: Any?): Boolean {
-        if(other is Student){
+        if(other is Student){ //'is' operator is the same as 'instance of'
             return other.id == this.id && other.name == this.name
         }
 
         return false
     }
+
+    override fun toString(): String {
+        return "Student(name=$name, id=$id)"
+    }
+
 
 }
